71b04b6ce3d303244e6a5a4a068f79fe7c1c0d40
mprog1701-code <m.prog1701@gmail.com>
2026-01-29 02:35:55 +0300

    13

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,6 +1,8 @@
 from core.models import SiteSettings
+from django.conf import settings
 
 def site_settings(request):
     """Context processor to make site settings available in all templates"""
     settings_obj, created = SiteSettings.objects.get_or_create(id=1)
-    return {'settings': settings_obj}
+    features = getattr(settings, 'FEATURE_FLAGS', {})
+    return {'settings': settings_obj, 'features': features}
