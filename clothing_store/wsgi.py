cebe1c6e5b8b02a07e1057428d5a4238efcec519
mprog1701-code <m.prog1701@gmail.com>
2026-03-04 03:05:40 +0300

    0-0-0-0-0-0

--- clothing_store/wsgi.py
+++ clothing_store/wsgi.py
@@ -1,27 +1,15 @@
 """
 WSGI config for clothing_store project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-module = os.environ.get('DJANGO_SETTINGS_MODULE') or ''
-if not module:
-    # Prefer production settings on cloud platforms (Render/Railway) by default
-    if os.environ.get('RAILWAY_ENVIRONMENT') or os.environ.get('RENDER'):
-        module = 'clothing_store.settings_production'
-    else:
-        module = 'clothing_store.settings'
-else:
-    # If referencing the removed module clothing_store.settings.production, fallback
-    if module.strip() == 'clothing_store.settings.production':
-        module = 'clothing_store.settings_production'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clothing_store.settings')
 
-try:
-    application = get_wsgi_application()
-except Exception:
-    # Final safe fallback
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'clothing_store.settings'
-    application = get_wsgi_application()
+application = get_wsgi_application()
